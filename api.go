bd64e572997ae0a2610e1c06762a4d8128516d49
jspc <james@zero-internet.org.uk>
2016-10-04 16:43:56 +0100

    Flesh out publish API

--- api.go
+++ api.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+    "encoding/json"
+    "fmt"
     "log"
     "net/http"
 )
@@ -36,13 +38,36 @@ func Router(w http.ResponseWriter, r *http.Request) {
 
         p := Project{FileName: handler.Filename, UUID: r.FormValue("uuid")}
         u := Uploader{Username: r.FormValue("username"), Password: r.FormValue("password"), Form: r.FormValue("form"), Project: &p}
-        u.UploadData(uploadFile)
+
+        if err := u.DumpToFS(); err != nil {
+            resp.Status = http.StatusInternalServerError
+            resp.Body.Message = err.Error()
+            resp.Body.Success = false
+            resp.respond(w)
+        }
+
+        defer u.CleanUp()
+
+        if status, err := u.UploadData(); err != nil {
+            resp.Status = http.StatusInternalServerError
+            resp.Body.Message = err.Error()
+            resp.Body.Success = false
+            resp.respond(w)
+        } else {
+            resp.Body.Message = status
+            resp.Body.Success = true
+        }
+        if err := u.CleanUp(); err != nil {
+            log.Printf("Error cleaning up: %s", err.Error())
+        }
 
     default:
         resp.Status = http.StatusNotFound
         resp.Body.Message = "Not found"
         resp.Body.Success = false
     }
+
+    resp.respond(w)
 }
 
 func LogRequest(r *http.Request) {
@@ -50,4 +75,10 @@ func LogRequest(r *http.Request) {
         r.RemoteAddr,
         r.Method,
         r.URL.Path)
+}
+
+func (r Response) respond (w http.ResponseWriter) {
+    w.WriteHeader(r.Status)
+    j,_ := json.Marshal(r)
+    fmt.Fprintf(w, string(j))
 }
