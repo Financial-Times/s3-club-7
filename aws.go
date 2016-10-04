0dcb82432ebb29b040d7c70fdcbfc17381e11b22
jspc <james@zero-internet.org.uk>
2016-10-04 16:42:36 +0100

    Put loaclfile to s3

--- aws.go
+++ aws.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-    "io"
+    "bytes"
+    "io/ioutil"
 
     "github.com/aws/aws-sdk-go/aws"
     "github.com/aws/aws-sdk-go/aws/session"
@@ -9,20 +10,30 @@ import (
 )
 
 var bucketName string
+var projectData []byte
+var output *s3.PutObjectOutput
+var err error
 
-func (u *Uploader)UploadData(projectData io.Reader)(string, error) {
-    svc := s3.New(session.New(), &aws.Config{Region: aws.String("eu-west-1")})
-
-    if bucketName, err := u.BucketName(); err != nil {
-        return "", err
+func (u *Uploader)UploadData()(out string, e error) {
+    if projectData, err = ioutil.ReadFile(u.tmpFile.Name()); err != nil {
+        e = err
+        return
     }
 
+    if bucketName, err = u.BucketName(); err != nil {
+        e = err
+        return
+    }
+
+    svc := s3.New(session.New(), &aws.Config{Region: aws.String("eu-west-1")})
     params := &s3.PutObjectInput{
         Bucket:             aws.String( bucketName ),
         Key:                aws.String( u.Project.Key() ),
-        Body:               projectData,
+        Body:               bytes.NewReader( projectData ),
     }
 
-    output, err := svc.PutObject(params)
-    return output.String(), err
+    output, e = svc.PutObject(params)
+    out = output.String()
+
+    return
 }
