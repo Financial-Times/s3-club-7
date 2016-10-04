0ff0b5d9cfad34151f7f20ec66232802b8734049
jspc <james@zero-internet.org.uk>
2016-10-04 16:43:21 +0100

    Make initialisation configurable

--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+    "flag"
     "log"
     "net/http"
 )
 
 var clusterid string
+var flexUrl string
+
+func init() {
+    clusterid = *flag.String("cluster", "staging", "flex cluster to run under")
+    flexUrl = *flag.String("flex-api", "flex.example.com/api", "Flex API Url to validate creds against")
+
+    flag.Parse()
+
+    if clusterid == "" {
+        log.Fatal("No flex cluster defined, please specify on the cli")
+    }
+}
 
 func main() {
-    clusterid = "jspcdev"
     log.Printf( "Creating projects under the %s cluster", clusterid )
-    log.Printf( "Listening on port %d", 8001 )
+    log.Printf( "Listening on port %d", 8000)
 
     http.HandleFunc("/", Router)
     http.ListenAndServe(":8000", nil)
